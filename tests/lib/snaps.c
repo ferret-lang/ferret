48f10e80969615bf5e73b865a26bd38f7dd3a93b
Ferret Lang <baraich.ferretlang@gmail.com>
2025-09-22 17:37:04 +0530

    chore: ability to generate and validate snapshots

--- tests/lib/snaps.c
+++ tests/lib/snaps.c
@@ -4,17 +4,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char *snapshot_extension = ".snap";
-size_t snapshot_directory_length = strlen(SNAPSHOTS_DIR);
+char *snapshot_status_string_equivalent(SnapshotStatus_e status_e) {
+  switch (status_e) {
+  case SNAPSHOT_STATUS_PENDING:
+    return "needs review";
+  case SNAPSHOT_STATUS_ACCEPTED:
+    return "accepted";
+  }
+}
 
-size_t get_snapshot_extension_length() { return strlen(snapshot_extension); }
+char *snapshot_extension_string_equivalent(SnapshotExtension_e extension_e) {
+  for (int i = 0; i < sizeof(snapshot_extensions) / sizeof(SnapshotExtension_t);
+       i++) {
+    if (snapshot_extensions[i].extension_e == extension_e) {
+      return snapshot_extensions[i].extension;
+    }
+  }
+  fprintf(stderr, "No extension found!");
+  exit(EXIT_FAILURE);
+}
 
 // Function to get the filepath to the snapshot file.
-static char *get_snapshot_filepath(const char *test_name) {
+char *get_snapshot_filepath(const char *test_name) {
   // Allocating memory for storing filepath.
+  char *snapshot_extension =
+      snapshot_extension_string_equivalent(SNAPSHOT_EXTENSION_DATA);
   char *filepath =
-      (char *)malloc(snapshot_directory_length + 1 + strlen(test_name) +
-                     get_snapshot_extension_length() + 1);
+      (char *)malloc(strlen(SNAPSHOTS_DIR) + 1 + strlen(test_name) +
+                     strlen(snapshot_extension) + 1);
   if (filepath == NULL) {
     fprintf(stderr, "Failed to allocate memory for the snap filepath.");
     exit(EXIT_FAILURE);
@@ -22,4 +39,62 @@ static char *get_snapshot_filepath(const char *test_name) {
 
   sprintf(filepath, "%s/%s%s", SNAPSHOTS_DIR, test_name, snapshot_extension);
   return filepath;
+}
+
+// Function to generate the snapshot file.
+bool snapshot_generate(const char *test_name, const char *data) {
+  char *snapshot_filepath = get_snapshot_filepath(test_name);
+
+  // Getting the pointer the file in writing mode.
+  FILE *snapshot_file = fopen(snapshot_filepath, "w");
+  if (snapshot_filepath == NULL) {
+    fprintf(stderr, "Failed to write the snapshot: %s\n", snapshot_filepath);
+    free(snapshot_filepath);
+    return false;
+  }
+
+  // Writing the snapshot data.
+  fprintf(snapshot_file, "%s", data);
+  fclose(snapshot_file);
+  return true;
+}
+
+// Function to compare the snapshot output.
+bool snapshot_validate(const char *test_name, const char *data) {
+  // Getting the path to the snapshot file on the system.
+  char *snapshot_filepath = get_snapshot_filepath(test_name);
+
+  // Open the file in reading mode.
+  FILE *snapshot_file = fopen(snapshot_filepath, "r");
+  if (snapshot_file == NULL) {
+    snapshot_generate(test_name, data);
+    fprintf(stderr, "Failed to open the snapshot file: %s\n",
+            snapshot_filepath);
+    free(snapshot_filepath);
+    return true;
+  }
+
+  // Reading the snapshot file.
+  fseek(snapshot_file, 0, SEEK_END);
+  size_t buffer_length = ftell(snapshot_file);
+  fseek(snapshot_file, 0, SEEK_SET);
+
+  char *snapshot = (char *)(malloc(buffer_length + 1));
+  if (snapshot == NULL) {
+    fprintf(stderr, "Failed to allocate memory for the snapshot contents: %s\n",
+            snapshot_filepath);
+    fclose(snapshot_file);
+    free(snapshot_filepath);
+    return false;
+  }
+  fread(snapshot, 1, buffer_length, snapshot_file);
+  snapshot[buffer_length] = '\0';
+
+  // Compare the snapshot.
+  bool result = strcmp(snapshot, data) == 0;
+  free(snapshot);
+  fclose(snapshot_file);
+  free(snapshot_filepath);
+
+  return result;
 }
--- tests/test_snaps.c
+++ tests/test_snaps.c
@@ -1,21 +1,44 @@
 #include "lib/snaps.c"
+#include "lib/snaps.h"
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stdlib.h>
 
 #include <cmocka.h>
 
+#define SNAPSHOT_TEST_DATA "Sample"
+
 static void test_snapshot_filepath_generation(void **state) {
-  char *filepath = get_snapshot_filepath("test_snapshot_filepath_generation");
+  char *filepath = get_snapshot_filepath(__func__);
   assert_string_equal("snaps/test_snapshot_filepath_generation.snap", filepath);
   free(filepath);
+}
+
+static void test_snapshot_generation(void **state) {
+  assert_int_equal(1, snapshot_generate(__func__, SNAPSHOT_TEST_DATA));
+  FILE *snapshot_file = fopen(get_snapshot_filepath(__func__), "r");
+  assert_ptr_not_equal(NULL, snapshot_file);
+  if (!(snapshot_file == NULL)) {
+    fclose(snapshot_file);
+  }
+}
+
+static void test_snapshot_validation(void **state) {
+  assert_int_equal(1, snapshot_validate(__func__, SNAPSHOT_TEST_DATA));
+}
+
+static void test_wrong_snapshot_validation(void **state) {
+  assert_int_equal(1, snapshot_validate(__func__, SNAPSHOT_TEST_DATA));
+  assert_int_not_equal(1, snapshot_validate(__func__, "Wrong Data"));
 }
 
 int main(void) {
   // Defining the test suite.
   const struct CMUnitTest tests[] = {
       cmocka_unit_test(test_snapshot_filepath_generation),
-  };
+      cmocka_unit_test(test_snapshot_generation),
+      cmocka_unit_test(test_snapshot_validation),
+      cmocka_unit_test(test_wrong_snapshot_validation)};
 
   // Running the tests.
   return cmocka_run_group_tests(tests, NULL, NULL);
