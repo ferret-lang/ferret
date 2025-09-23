695fad414623bd4a4a81bb382ea2b02ca83f9959
Ferret Lang <baraich.ferretlang@gmail.com>
2025-09-23 12:44:03 +0530

    chore: refactored snaps for safer code

--- tests/lib/snaps.c
+++ tests/lib/snaps.c
@@ -43,60 +43,75 @@ char *generate_filepath(const char *test_name,
 }
 
 // Function to generate the snapshot file.
-bool snapshot_update(const char *filepath, const char *data) {
+bool snapshot_update(const char *test_name, const char *data) {
+  char *filepath = generate_filepath(test_name, SNAPSHOT_EXTENSION_DATA);
   // Getting the pointer the file in writing mode.
   FILE *snapshot_file = fopen(filepath, "w");
   if (snapshot_file == NULL) {
     fprintf(stderr, "Failed to write the snapshot: %s\n", filepath);
+    free(filepath);
     return false;
   }
 
   // Writing the snapshot data.
   fprintf(snapshot_file, "%s", data);
   fclose(snapshot_file);
+  free(filepath);
   return true;
 }
 
-bool snapshot_status_update(const char *filepath, SnapshotStatus_e status) {
+bool snapshot_status_update(const char *test_name, SnapshotStatus_e status) {
+  char *filepath = generate_filepath(test_name, SNAPSHOT_EXTENSION_STATUS);
   FILE *snapshot_status_file = fopen(filepath, "w");
   if (snapshot_status_file == NULL) {
     fprintf(stderr, "Failed to write the snapshot status: %s", filepath);
+    free(filepath);
     return false;
   }
 
   fprintf(snapshot_status_file, "%s",
           snapshot_status_string_equivalent(status));
   fclose(snapshot_status_file);
+  free(filepath);
   return true;
 }
 
+// Function to check if the snapshot file exists.
+bool snapshot_exists(const char *test_name, SnapshotExtension_e extension_e) {
+  char *filepath = generate_filepath(test_name, extension_e);
+  if (!file_exists(filepath)) {
+    free(filepath);
+    return false;
+  }
+  free(filepath);
+  return true;
+}
+
+char *get_snapshot_contents(const char *test_name,
+                            SnapshotExtension_e extension_e) {
+  char *filepath = generate_filepath(test_name, extension_e);
+  char *file_contents = get_file_contents(filepath);
+  free(filepath);
+  return file_contents;
+}
+
 // Function to compare the snapshot output.
-
 bool snapshot_validate(const char *test_name, const char *data) {
-  // Getting the path to the snapshot file on the system.
-  char *snapshot_filepath =
-      generate_filepath(test_name, SNAPSHOT_EXTENSION_DATA);
-  char *snapshot_status_filepath =
-      generate_filepath(test_name, SNAPSHOT_EXTENSION_STATUS);
-
-  if (!file_exists(snapshot_filepath)) {
-    snapshot_update(snapshot_filepath, data);
-    snapshot_status_update(snapshot_status_filepath, SNAPSHOT_STATUS_PENDING);
-    free(snapshot_filepath);
-    free(snapshot_status_filepath);
+  if (!snapshot_exists(test_name, SNAPSHOT_EXTENSION_DATA)) {
+    snapshot_update(test_name, data);
+    snapshot_status_update(test_name, SNAPSHOT_STATUS_PENDING);
     return false;
   }
 
-  if (!file_exists(snapshot_status_filepath)) {
-    snapshot_status_update(snapshot_status_filepath, SNAPSHOT_STATUS_PENDING);
-    free(snapshot_filepath);
-    free(snapshot_status_filepath);
+  if (!snapshot_exists(test_name, SNAPSHOT_EXTENSION_STATUS)) {
+    snapshot_status_update(test_name, SNAPSHOT_STATUS_PENDING);
     return false;
   }
 
   // Reading the files.
-  char *snapshot = get_file_contents(snapshot_filepath);
-  char *snapshot_status = get_file_contents(snapshot_status_filepath);
+  char *snapshot = get_snapshot_contents(test_name, SNAPSHOT_EXTENSION_DATA);
+  char *snapshot_status =
+      get_snapshot_contents(test_name, SNAPSHOT_EXTENSION_STATUS);
 
   // Strip any trailing newlines or spaces from snapshot_status
   char *status_trimmed = strdup(snapshot_status);
@@ -108,22 +123,13 @@ bool snapshot_validate(const char *test_name, const char *data) {
     free(status_trimmed);
     free(snapshot);
     free(snapshot_status);
-    free(snapshot_filepath);
-    free(snapshot_status_filepath);
     return false;
   }
 
   // Compare the snapshot data
   bool result = (strcmp(snapshot, data) == 0);
-  if (!result) {
-    snapshot_update(snapshot_filepath, data);
-    snapshot_status_update(snapshot_status_filepath, SNAPSHOT_STATUS_PENDING);
-  }
-
   free(status_trimmed);
   free(snapshot);
   free(snapshot_status);
-  free(snapshot_filepath);
-  free(snapshot_status_filepath);
   return result;
 }
